fb6f10f5753899f228be9a4159a1fb3262c7f6a3
adaynygmanov <adaynygmanov@gmail.com>
2020-04-08 16:46:10 +0600

    TSLA commit

---
+++ TSLA.R
@@ -0,0 +1,42 @@
+## Load libraries
+library (quantmod)
+
+## Load data
+loadSymbols(c("TSLA"))
+head (TSLA)
+
+## Build charts with trends
+chartSeries(TSLA) 
+chartSeries(TSLA, multi.col = T,theme = "white", subset = "2015-4::2020-4")
+addSMA() #Moving Average
+addMACD() #Moving Average convergence/divergence
+addBBands()#BollingerBands
+addCCI() # Commodity Channel Index
+addADX()# Average Directional Movement Index
+addCMF() #Chaikin Money Flow
+addRSI() # Relative Strength Index
+
+seriesHi(TSLA) #highest prices
+seriesLo(TSLA) #lowest prices
+
+## Optional scripts
+only_2020_values<-TSLA ['2020']
+head(only_2020_values);tail(only_2020_values)
+data_from_2017_onwards<-TSLA ['2017::']
+head(data_from_2017_onwards);tail(data_from_2017_onwards)
+
+## Analysis of Returns
+returns_by_day<-dailyReturn(TSLA)
+head(returns_by_day); tail(returns_by_day)
+
+returns_by_week<-weeklyReturn(TSLA)
+head(returns_by_week); tail(returns_by_week)
+
+returns_by_month<-monthlyReturn(TSLA)
+head(returns_by_month); tail(returns_by_month)
+
+returns_by_year<-yearlyReturn(TSLA)
+head(returns_by_year); tail(returns_by_year)
+
+TSLA_all_returns<-allReturns(TSLA)
+head(TSLA_all_returns); tail(TSLA_all_returns)
